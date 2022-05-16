5f6f71cfb9c84e3b685ead3aedf47286242b4e9c
Filip Mikus <xmikusf@stuba.sk>
2022-05-16 22:36:30 +0200

    feat: logs to DB via PDO

---
+++ server/PDOLogsManager.php
@@ -0,0 +1,63 @@
+<?php
+
+class PDOLogsManager {
+    private string $serverName;
+    private string $userName;
+    private string $userPassword;
+    private string $databaseName;
+
+    public function __construct($serverName, $userName, $userPassword, $databaseName) {
+        $this->serverName = $serverName;
+        $this->userName = $userName;
+        $this->userPassword = $userPassword;
+        $this->databaseName = $databaseName;
+    }
+
+    public function readAllLogs() {
+        try {
+            $connection = new PDO(
+                "mysql:host=$this->serverName;dbname=$this->databaseName",
+                $this->userName,
+                $this->userPassword
+            );
+            $statement = $connection->prepare(
+                "SELECT * FROM cas_logs");
+            $statement->execute();
+            $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $statement->fetchAll();
+
+            $connection = null;
+        } catch (PDOException $exception) {
+            return null;
+        }
+        return $result;
+    }
+
+    public function createLog($command, $status) {
+        try {
+            $connection = new PDO(
+                "mysql:host=$this->serverName;dbname=$this->databaseName",
+                $this->userName,
+                $this->userPassword
+            );
+
+            $connection->beginTransaction();
+            $statement = $connection->prepare(
+                "INSERT INTO cas_logs(command, status) VALUES(?, ?)");
+            $statement->execute([$command, $status]);
+            $lastInsertId = $connection->lastInsertId();
+            $connection->commit();
+
+            $statement = $connection->prepare(
+                "SELECT * FROM cas_logs WHERE id = ?");
+            $statement->execute([$lastInsertId]);
+            $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $statement->fetch();
+
+            $connection = null;
+        } catch (PDOException $exception) {
+            return null;
+        }
+        return $result;
+    }
+}
