c3af3f89cb7b759f49e862974eb4d8021048dd81
Filip Mikus <xmikusf@stuba.sk>
2022-05-16 17:27:20 +0200

    add: config.php

---
+++ server/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    "serverName" => "localhost",
+    "userName" => "xmikusf",
+    "userPassword" => "kT6gKrVf8NQkk9C",
+    "databaseName" => "skuskove_zadanie",
+    "logsEmail" => "filip.mikus11@gmail.com"
+);
