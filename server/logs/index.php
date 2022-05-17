07689710e43c8f6375a503e99d5c72895be39c46
tomaskomora <xkomorat@stuba.sk>
2022-05-17 22:25:26 +0200

    feat: implement logs API endpoint

--- server/logs/index.php
+++ server/logs/index.php
@@ -1,2 +1,19 @@
 <?php
-//TODO logs API endpoint
+header('Content-Type: application/json; charset=utf-8');
+require_once "../PDOLogsManager.php";
+
+$config = require_once "../config.php";
+$serverName = $config["host"];
+$userName = $config["username"];
+$userPassword = $config["password"];
+$databaseName = $config["dbname"];
+
+$pdoLogsManager = new PDOLogsManager($serverName, $userName, $userPassword, $databaseName);
+$result = $pdoLogsManager->readAllLogs();
+if ($result != null) {
+    header("HTTP/1.1 200 OK");
+    echo json_encode($result);
+} else {
+    header("HTTP/1.1 404 Not Found");
+    echo "{}";
+}
