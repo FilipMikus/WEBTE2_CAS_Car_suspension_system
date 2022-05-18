0b31e1f0a7c6e72e8ae3a09b3661af7f89261b3c
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2022-05-18 15:17:54 +0200

    feat: implement translations API endpoint implemented

--- server/translations/index.php
+++ server/translations/index.php
@@ -1,2 +1,24 @@
 <?php
-//TODO translations API endpoint
+header('Content-Type: application/json; charset=utf-8');
+require_once "../PDOTranslationsManager.php";
+
+$config = require_once "../config.php";
+$serverName = $config["host"];
+$userName = $config["username"];
+$userPassword = $config["password"];
+$databaseName = $config["dbname"];
+
+if (isset($_GET["language"])) {
+    $pdoTranslationsManager = new PDOTranslationsManager($serverName, $userName, $userPassword, $databaseName);
+    $result = $pdoTranslationsManager->readAllByLanguageCode($_GET["language"]);
+    if ($result != null) {
+        header("HTTP/1.1 200 OK");
+        echo json_encode($result);
+    } else {
+        header("HTTP/1.1 404 Not Found");
+        echo "{}";
+    }
+} else {
+    header("HTTP/1.1 404 Not Found");
+    echo "{}";
+}
